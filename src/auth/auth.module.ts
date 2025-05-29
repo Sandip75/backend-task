e10bbe65950ef7bb94b97f0b7649ccb384022edb
sk-carrot <sandip.kumar@carrot.club>
2025-05-30 01:09:29 +0530

    login changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1200s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { SignupDto } from './dto/signup.dto';
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     private prisma = new PrismaClient();
+    constructor(private readonly jwtService: JwtService) { }
 
     async signup(dto: SignupDto) {
         const existing = await this.prisma.member.findUnique({
@@ -15,7 +18,6 @@ export class AuthService {
         if (existing) {
             throw new BadRequestException('Email already registered');
         }
-console.log('Received password:', dto.password);
 
         const hashedPassword = await bcrypt.hash(dto.password, 10);
 
@@ -32,5 +34,25 @@ console.log('Received password:', dto.password);
             username: user.username,
             createdAt: user.createdAt.toISOString(),
         };
+    }
+
+    async login(dto: LoginDto) {
+        const user = await this.prisma.member.findUnique({
+            where: { id: dto.id },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        const payload = { sub: user.id };
+        const token = await this.jwtService.signAsync(payload);
+
+        return { access_token: token };
     }
 }
